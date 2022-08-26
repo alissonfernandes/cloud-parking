829c9d2a73af7856a01a942d427c62107db5ce9d
alissonfernandes <alissonfernandes200@gmail.com>
2022-08-25 22:03:43 -0300

    create Vehicle DTO

---
+++ src/main/java/br/com/alissonfernandes/cloudparking/dto/VehicleDTO.java
@@ -0,0 +1,24 @@
+package br.com.alissonfernandes.cloudparking.dto;
+
+import br.com.alissonfernandes.cloudparking.enums.VehicleType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VehicleDTO {
+
+    private long id;
+    private VehicleType vehicleType;
+    private String placa;
+    private String color;
+    private LocalDateTime entryDate;
+    private LocalDateTime exitDate;
+    private Double bill;
+    private String driveName;
+    private VacancyDTO vacancyDTO;
+}
