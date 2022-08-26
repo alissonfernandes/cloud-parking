c422893706cfb95731326cc0f743f8289a9842b9
alissonfernandes <alissonfernandes200@gmail.com>
2022-08-25 22:20:32 -0300

    create Vehicle controller

---
+++ src/main/java/br/com/alissonfernandes/cloudparking/controller/VehicleController.java
@@ -0,0 +1,47 @@
+package br.com.alissonfernandes.cloudparking.controller;
+
+import br.com.alissonfernandes.cloudparking.dto.VehicleDTO;
+import br.com.alissonfernandes.cloudparking.service.IVehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/vehicle")
+public class VehicleController {
+
+    @Autowired
+    private IVehicleService vehicleService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public VehicleDTO createVehicle(@RequestBody VehicleDTO vehicleDTO) {
+        return vehicleService.create(vehicleDTO);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public VehicleDTO get(Long id) {
+        return vehicleService.get(id);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<VehicleDTO> getListAll() {
+        return vehicleService.listAll();
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.UPGRADE_REQUIRED)
+    public VehicleDTO updateVehicle(@PathVariable Long id, @RequestBody VehicleDTO vehicleDTO) {
+        return vehicleService.update(id, vehicleDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteVehicle(@PathVariable Long id) {
+        vehicleService.delete(id);
+    }
+}
