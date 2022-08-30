f99d6f9069de93e4dafa2a6c88b38522585c341b
alissonfernandes <alissonfernandes200@gmail.com>
2022-08-29 21:03:07 -0300

    test method POST in VacancyController

---
+++ src/test/java/br/com/alissonfernandes/cloudparking/controller/VacancyControllerTest.java
@@ -0,0 +1,77 @@
+package br.com.alissonfernandes.cloudparking.controller;
+
+import br.com.alissonfernandes.cloudparking.builder.VacancyDTOBuilder;
+import br.com.alissonfernandes.cloudparking.dto.VacancyDTO;
+import br.com.alissonfernandes.cloudparking.service.IVacancyService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doNothing;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.doThrow;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.empty;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class VacancyControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Mock
+    IVacancyService vacancyService;
+
+    @InjectMocks
+    VacancyController vacancyController;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(vacancyController)
+                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+                .setViewResolvers((s, locale) -> new MappingJackson2JsonView())
+                .build();
+    }
+
+    private static final String VACANCY_API_URL_PATH = "/api/v1/vacancy";
+    private static final long VALID_VACANCY_ID = 1L;
+
+    @Test
+    @DisplayName("quando POST é chamado então um vacancy é criado")
+    void whenPOSTIsCalledThenAVacancyIsCreated() throws Exception {
+        VacancyDTO vacancyDTO = VacancyDTOBuilder.builder().build().toVacancyDTO();
+
+        when(vacancyService.create(vacancyDTO)).thenReturn(vacancyDTO);
+
+        mockMvc.perform(post(VACANCY_API_URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(vacancyDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id", is(not(empty()))))
+                .andExpect(jsonPath("$.status", is(vacancyDTO.getStatus().getStatus().toUpperCase())))
+                .andExpect(jsonPath("$.vehicleType", is(vacancyDTO.getVehicleType().getType().toUpperCase())));
+    }
+
+
+}
