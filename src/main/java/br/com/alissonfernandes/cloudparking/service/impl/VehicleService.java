63ab6e1ece732e48f805f5c52fd340f83545ccb2
alissonfernandes <alissonfernandes200@gmail.com>
2022-09-07 22:03:11 -0300

    delete 'IVehicleService' interface file and rename 'VehicleService'

--- src/main/java/br/com/alissonfernandes/cloudparking/service/impl/VehicleService.java
+++ src/main/java/br/com/alissonfernandes/cloudparking/service/impl/VehicleService.java
@@ -1,12 +1,10 @@
 package br.com.alissonfernandes.cloudparking.service.impl;
 
 import br.com.alissonfernandes.cloudparking.dto.VehicleDTO;
-import br.com.alissonfernandes.cloudparking.enums.VehicleAction;
 import br.com.alissonfernandes.cloudparking.exception.VehicleNotFoundException;
 import br.com.alissonfernandes.cloudparking.mapper.VehicleMapper;
 import br.com.alissonfernandes.cloudparking.model.Vehicle;
 import br.com.alissonfernandes.cloudparking.repository.VehicleRepository;
-import br.com.alissonfernandes.cloudparking.service.IVehicleService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,51 +15,51 @@ import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor(onConstructor = @__(@Autowired))
-public class VehicleServiceImpl implements IVehicleService {
+public class VehicleService {
 
     private VehicleRepository vehicleRepository;
 
     private VehicleMapper vehicleMapper;
 
-    @Override
-    public VehicleDTO create(VehicleDTO vehicleDTO) {
+    public VehicleDTO entry(VehicleDTO vehicleDTO) {
+        vehicleDTO.setEntryDate(LocalDateTime.now());
         return this.vehicleSave(vehicleDTO);
     }
 
-    @Override
-    public VehicleDTO get(Long id) throws VehicleNotFoundException {
-       Vehicle vehicleReturned = this.verifyIfExists(id);
-       return vehicleMapper.toDTO(vehicleReturned);
+    public VehicleDTO exit(Long id) throws VehicleNotFoundException {
+        VehicleDTO vehicleDTO = this.verifyIfExists(id);
+        vehicleDTO.setExitDate(LocalDateTime.now());
+        return this.vehicleSave(vehicleDTO);
     }
 
-    @Override
+    public VehicleDTO get(Long id) throws VehicleNotFoundException {
+       VehicleDTO vehicleReturned = this.verifyIfExists(id);
+       return vehicleReturned;
+    }
+
     public List<VehicleDTO> listAll() {
         List<Vehicle> vehicles = vehicleRepository.findAll();
         return vehicles.stream().map(v -> vehicleMapper.toDTO(v)).collect(Collectors.toList());
     }
 
-    @Override
     public VehicleDTO update(Long id, VehicleDTO vehicleDTO) throws VehicleNotFoundException {
         this.verifyIfExists(id);
         return this.vehicleSave(vehicleDTO);
     }
 
-    @Override
-    public void delete(Long id) throws VehicleNotFoundException {
-        this.verifyIfExists(id);
-        vehicleRepository.deleteById(id);
+    public VehicleDTO delete(Long id) throws VehicleNotFoundException {
+        VehicleDTO vehicle = this.verifyIfExists(id);
+        vehicle.setExitDate(LocalDateTime.now());
+        return this.vehicleSave(vehicle);
     }
 
-    private Vehicle verifyIfExists(Long id) throws VehicleNotFoundException {
+    private VehicleDTO verifyIfExists(Long id) throws VehicleNotFoundException {
         Vehicle vehicle = vehicleRepository.findById(id)
                 .orElseThrow(() -> new VehicleNotFoundException(id));
-        return vehicle;
+        return vehicleMapper.toDTO(vehicle);
     }
 
     private VehicleDTO vehicleSave(VehicleDTO vehicleDTO) {
-        if (vehicleDTO.getVehicleAction() == VehicleAction.ENTRY) vehicleDTO.setEntryDate(LocalDateTime.now());
-        else if(vehicleDTO.getVehicleAction() == VehicleAction.EXIT) vehicleDTO.setExitDate(LocalDateTime.now());
-
         Vehicle vehicletoSalve = vehicleMapper.toModel(vehicleDTO);
         Vehicle vehicleSaved = vehicleRepository.save(vehicletoSalve);
 
