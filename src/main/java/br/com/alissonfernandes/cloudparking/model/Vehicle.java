9ab745241551ebd2a0c43309e702a56d20e4cf87
alissonfernandes <alissonfernandes200@gmail.com>
2022-12-11 15:48:51 -0300

    update models and create parking DAO

--- src/main/java/br/com/alissonfernandes/cloudparking/model/Vehicle.java
+++ src/main/java/br/com/alissonfernandes/cloudparking/model/Vehicle.java
@@ -2,6 +2,7 @@ package br.com.alissonfernandes.cloudparking.model;
 
 import br.com.alissonfernandes.cloudparking.enums.VehicleType;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Vehicle {
 
     @Id
---
+++ src/main/java/br/com/alissonfernandes/cloudparking/repository/ParkingRepository.java
@@ -0,0 +1,9 @@
+package br.com.alissonfernandes.cloudparking.repository;
+
+import br.com.alissonfernandes.cloudparking.model.Parking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParkingRepository extends JpaRepository<Parking, Long> {
+}
