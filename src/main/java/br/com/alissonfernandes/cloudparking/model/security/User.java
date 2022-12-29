1cbd39dfb9729435981d3377a30babec85f30b74
alissonfernandes <alissonfernandes200@gmail.com>
2022-12-29 09:06:18 -0300

    create CRUD User database

---
+++ src/main/java/br/com/alissonfernandes/cloudparking/model/security/User.java
@@ -0,0 +1,38 @@
+package br.com.alissonfernandes.cloudparking.model.security;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "tab_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Integer id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(length = 20, nullable = false)
+    private String username;
+
+    @Column(length = 100, nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "tab_user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role_id")
+    private List<String> roles = new ArrayList<>();
+
+
+    private User(String username) {
+        this.username = username;
+    }
+}
