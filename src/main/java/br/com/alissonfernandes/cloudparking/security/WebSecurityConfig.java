06d5bd52597aabeb4c98ff490bb904a0078ca0f3
alissonfernandes <alissonfernandes200@gmail.com>
2022-12-29 09:45:05 -0300

    save users in database and validate user

--- src/main/java/br/com/alissonfernandes/cloudparking/security/WebSecurityConfig.java
+++ src/main/java/br/com/alissonfernandes/cloudparking/security/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package br.com.alissonfernandes.cloudparking.security;
 
+import br.com.alissonfernandes.cloudparking.service.security.SecurityDatabaseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,11 +9,20 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity // habilitando segurança web manual
 @EnableGlobalMethodSecurity(prePostEnabled = true)//
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private SecurityDatabaseService securityService;
+
+    @Autowired
+    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(securityService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -23,23 +34,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.GET, METHOD_GET_ROLE_USERS).hasRole("USERS")
                 .antMatchers(HttpMethod.POST, "/entry").hasRole("USERS")
-                .antMatchers(HttpMethod.POST, "/vacancy").hasRole("MANAGERS")
+                .antMatchers(HttpMethod.POST, "/api/v1/vacancy").hasRole("MANAGERS")
                 .antMatchers(HttpMethod.PUT, METHOD_PUT_ROLE_MANAGERS).hasRole("MANAGERS")
                 .antMatchers(HttpMethod.DELETE, METHOD_DELETE_ROLE_MANAGERS).hasRole("MANAGERS")
                 .anyRequest().authenticated().and().httpBasic();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password("{noop}123")
-                .roles("USERS")
-                .and()
-                .withUser("master")
-                .password("{noop}master123")
-                .roles("MANAGERS");
     }
 
 }
